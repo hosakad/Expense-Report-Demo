ddab98956b702c85d9191aa0397e4783ea43db71
hosakad <daisuke.hosaka@pendo.io>
2021-03-31 14:57:28 +0900

    Update expense_report_demo.py

--- expense_report_demo.py
+++ expense_report_demo.py
@@ -91,7 +91,7 @@ def error(message):
 def login():
 	return render_template('login.html')
 
-@app.route('/authenticate')
+@app.route('/authenticate', method=['POST'])
 def authenticate():
 
 	email = request.form['email']
