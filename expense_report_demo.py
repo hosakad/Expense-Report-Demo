59aa3d8cd94b05e916d70e68bdd5c0f0bc692ec2
hosakad <daisuke.hosaka@pendo.io>
2021-04-07 22:31:41 +0900

    Update expense_report_demo.py

--- expense_report_demo.py
+++ expense_report_demo.py
@@ -292,7 +292,7 @@ def update_report():
 		# add specified expenses to this report
 		sql_string = "update expense set"\
 								" expense.report_id = '"+request.form['id']+"'"\
-								" where expense.id in'"+id_added+"'"
+								" where expense.id in("+",".join(id_added)+")"
 		sql_create_update(sql_string)
 
 	id_removed = request.form.getlist('id_removed')
@@ -300,7 +300,7 @@ def update_report():
 		# remove specified expenses from this report
 		sql_string = "update expense set"\
 								" expense.report_id = null"\
-								" where expense.id in'"+id_removed+"'"
+								" where expense.id in("+",".join(id_removed)+")"
 		sql_create_update(sql_string)
 
 	return redirect(url_for('report_html'))
