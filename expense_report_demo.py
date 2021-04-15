69ca4ff42aebf56364bbb4f06ed637b4fa1c9ebe
hosakad <daisuke.hosaka@pendo.io>
2021-04-15 19:28:02 +0900

    Update expense_report_demo.py

--- expense_report_demo.py
+++ expense_report_demo.py
@@ -125,8 +125,9 @@ def get_language(language):
 def get_message_dict():
 	# load messages
 	messages = {}
-	path = '../'+url_for('static', filename='json/messages_'+redis_client.get(REDIS_LANGUAGE).decode('utf8'))+'.json'
+	path = '..'+url_for('static', filename='json/messages_'+redis_client.get(REDIS_LANGUAGE).decode('utf8'))+'.json'
 	print('path:', path)
+	print('current path:', os.getcwd())
 	try:
 		with open(path) as message_file:
 			messages = json.load(message_file)
